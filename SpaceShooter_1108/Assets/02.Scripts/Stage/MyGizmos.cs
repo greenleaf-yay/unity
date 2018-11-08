134e075cf186b9339df3c0cd122e7bc38b32d38f
greenleaf-yay <greenleaf.yay@gmail.com>
2018-11-08 15:10:41 +0900

    SS_181108

---
+++ SpaceShooter_1108/Assets/02.Scripts/Stage/MyGizmos.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MyGizmos : MonoBehaviour
+{
+    public Color _color = Color.yellow;
+    public float _radius = 0.1f;
+    public enum Type { NORMAL, WAYPOINT };
+    public Type _type = Type.NORMAL;
+
+    private const string wayPointFile = "Enemy";
+
+    private void OnDrawGizmos()
+    {
+        if (_type == Type.NORMAL)
+        {
+            Gizmos.color = _color;
+            Gizmos.DrawSphere(transform.position, _radius);
+        }
+        else
+        {
+            Gizmos.color = _color;
+            Gizmos.DrawIcon(transform.position + Vector3.up
+                * 1.0f, wayPointFile, true);
+            Gizmos.DrawWireSphere(transform.position, _radius);
+        }        
+    }
+}
