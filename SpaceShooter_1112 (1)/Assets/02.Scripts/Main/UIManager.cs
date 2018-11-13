b89fc22962be9fe73fdc45dde3ec0fc1346fdba2
greenleaf-yay <greenleaf.yay@gmail.com>
2018-11-13 12:56:21 +0900

    SS_181113

---
+++ SpaceShooter_1112 (1)/Assets/02.Scripts/Main/UIManager.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class UIManager : MonoBehaviour
+{
+    public void OnClickStartBtn( )
+    {
+        //Debug.Log("Click START Button : " + msg);
+        SceneManager.LoadScene("Level1"); // 해당 씬 로드
+        SceneManager.LoadScene("scPlay", LoadSceneMode.Additive); // Level1 로드 후 scPlay 추가
+    }
+}
---
+++ SpaceShooter_1113/Assets/02.Scripts/Player/LaserBeam.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LaserBeam : MonoBehaviour
+{
+    private Transform tr;
+    private LineRenderer line;
+    private RaycastHit hit;
+    private float fireRate;
+    private float nextFire = 0.0f;
+
+    void Start()
+    {
+        tr = GetComponent<Transform>();
+        line = GetComponent<LineRenderer>();
+        //line.useWorldSpace = false;
+        line.enabled = false;
+        line.startWidth = 0.1f;
+        line.endWidth = 0.05f;
+        fireRate = GetComponentInParent<FireCtrl>().fireRate;
+    }
+
+    void Update()
+    {
+        Ray ray = new Ray(tr.position, tr.forward);
+        if (Input.GetMouseButton(0))
+        {
+            if (Time.time >= nextFire)
+            {
+                //line.SetPosition(0
+                //    , tr.InverseTransformPoint(ray.origin));
+                line.SetPosition(0, tr.position);
+                if (Physics.Raycast(ray, out hit, 100.0f))
+                {
+                    //line.SetPosition(1
+                    //    , tr.InverseTransformPoint(hit.point));
+                    line.SetPosition(1, hit.point);
+                }
+                //else line.SetPosition(1
+                //, tr.InverseTransformPoint(ray.GetPoint(100.0f)));
+                else line.SetPosition(1, ray.GetPoint(100.0f));
+                StartCoroutine(this.ShowLaserBeam());
+                nextFire = Time.time + fireRate;
+            }
+        }
+    }
+
+    IEnumerator ShowLaserBeam()
+    {
+        line.enabled = true;
+        yield return new WaitForSeconds(Random.Range(0.01f, 0.2f));
+        line.enabled = false;
+    }
+}
